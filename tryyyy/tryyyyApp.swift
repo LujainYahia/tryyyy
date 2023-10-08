a5e47903468f0aef8513f66045d1382033e91949
lujain <luuujainnn@gmail.com>
2023-10-08 11:21:11 +0300

    Initial Commit

---
+++ tryyyy/tryyyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryyyyApp.swift
+//  tryyyy
+//
+//  Created by Lujain Yhia on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct tryyyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
